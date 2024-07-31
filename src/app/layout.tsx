414b22f25a2e033b12613bfffa19d2c4535a8359
anhkhoi0606 <anhkhoi20.akin@gmail.com>
2024-07-31 18:42:19 +0700

    task2_nextJS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Head from 'next/head'; // Import Head từ next/head
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,6 +17,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+          integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,85 @@
 import Image from "next/image";
+import "./page.css"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+  <div className="main">
+    <div className='header'>
+      <h1 className='header_title'>TODO LIST</h1>
+        <div className='header_search-bar'>
+          <div className='header_search-bar-box'>
+            <input className='header_search-bar-input' placeholder='Search note...'></input>
+            <i className="fa-solid fa-magnifying-glass header_search-bar-icon"></i>
+          </div>
+         <div className='header_select color-hover'>
+          All
+          <i className="fa-solid fa-angle-down"></i>
+          <div className='header_select-list'>
+            <p className='header_select-list-items'>All</p>
+            <p className='header_select-list-items'>Complete</p>
+            <p className='header_select-list-items'>Incomplete</p>
+          </div>
+          </div>
+          <button className='header_btn-mode color-hover'>
+          <i className="fa-regular fa-moon"></i>
+          </button>
+
       </div>
+    </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <ul className='content_list'>
+            <li className='content_list_note'>
+              <div className='content_note'>
+                <input className='content_note-checkbox' type= "checkbox"></input>   
+                <span className='content_note-text'>
+                NOTE #1 
+                </span>
+              </div>
+              <div className='content_handle'>
+                <p className="content_handle-icon content_handle-icon--edit"><i className="fa-solid fa-pen"></i></p>
+                <p className="content_handle-icon content_handle-icon--delete"><i className="fa-solid fa-trash"></i></p>
+              </div>
+            </li>
+            <li className='content_list_note'>
+              <div className='content_note'>
+                <input className='content_note-checkbox' type= "checkbox"></input>   
+                <span className='content_note-text content_note-text--delete'>
+                NOTE #2 
+                </span>
+              </div>
+              <div className='content_handle'>
+              <p className="content_handle-icon content_handle-icon--edit"><i className="fa-solid fa-pen"></i></p>
+              <p className="content_handle-icon content_handle-icon--delete"><i className="fa-solid fa-trash"></i></p>
+              </div>
+            </li>
+            <li className='content_list_note'>
+              <div className='content_note'>
+                <input className='content_note-checkbox' type= "checkbox"></input>   
+                <span className='content_note-text'>
+                NOTE #3 
+                </span>
+              </div>
+              <div className='content_handle'>
+              <p className="content_handle-icon content_handle-icon--edit"><i className="fa-solid fa-pen"></i></p>
+              <p className="content_handle-icon content_handle-icon--delete"><i className="fa-solid fa-trash"></i></p>
+              </div>
+            </li>
+          </ul>
+          <div className='content_add color-hover'>
+          <i className="fa-solid fa-plus"></i>
+          <div className='page_add_note'>
+              <h1 className='header_title'>NEW NOTE</h1>
+              <div className='content_input'>
+               <input className='content_input-add' placeholder='Input your note...'></input>
+              </div>
+              <div className='footer'>
+                <button className='footer_btn footer_btn-cancel'>CANCEL</button>
+                <button className='footer_btn footer_btn-apply color-hover'>APPLY</button>
+              </div>
+            </div>
+          </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+  </div>
   );
 }
